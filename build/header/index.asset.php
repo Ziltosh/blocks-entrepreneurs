1098b9fcd678dca3172be7016d1e012bdc5c8d7f
nicolas <zeross30@gmail.com>
2024-08-12 18:11:51 +0200

    Mise a jour blocks

--- build/header/index.asset.php
+++ build/header/index.asset.php
@@ -1,1 +1,1 @@
-<?php return ["dependencies" => ["wp-blocks","wp-block-editor","wp-element"], "version" => "0e62f11b82e7131286d375e170f88d18"];
+<?php return ["dependencies" => ["wp-blocks","wp-block-editor","wp-element"], "version" => "dfd10bc4f32f86ad553385b2c991e1d7"];
